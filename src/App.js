1015183e04993d19970ab1a8258577feb7000fac
aditya verma <adityavermasuper.12527@gmail.com>
2023-05-04 10:31:07 +0530

    otp generator by post fetch

--- src/App.js
+++ src/App.js
@@ -1,32 +1,64 @@
+import { useState } from 'react';
 import './App.css';
-import { FilePicker } from './useeffect';
 
 function App() {
+ const [mobilenumber,setMobilenumber] =useState()
+  
+ function handle(){
+  if(mobilenumber < 999967896 || mobilenumber >= 56567890342){
+    alert ("Invalid mobile number")
+    console.error("invalid number")
+    return;
+  }
+  fetch( "https://cdn-api.co-vin.in/api/v2/auth/public/generateOTP",{
+    method:"POST",
+    headers:{
+      "content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      mobile: mobilenumber
+    })
+  })
 
-  //  components -/> small isolated piece of code of you complete ui .
-  // components  -/> functions (react function)
-  //  js function -/> takes datatype as input and return data types
-  // react function -/> takes props as input and return jsx / html likw components .
-  //  react functions startWith Capital letter only . 
-
-
-//   Jsx -/> javascript extension 
-//   it provides the power to write the html in js file 
-
-//  ui 
-//  functionality 
-//  data management 
-//  folder structure (Atomic folder structure)
-
-
-//  css  -> style to style the component
-//  js -/> code to ceate the component 
-
-
-
+     .then((res)=> res.json())
+     .then((data)=> {
+      setMobilenumber(" ");
+      alert("succesfully")
+      console.log(data);
+     })
+     .catch(error=>{
+      alert("plase retry, somthing went wrong")
+      console.error(error);
+     });
+      }
   return (
-    <div className="App">
-    <FilePicker/>
+    <div className='App'>
+      <form
+      onSubmit={e=> e.preventDefault()}
+      noValidate
+      autoComplete='off'
+      
+      >
+        <input type="number" placeholder='mobile number' 
+        validation={{
+          required:{
+            value:"true",
+            message:"required",
+          },
+          minLength:{
+            value:10,
+            message: "min 10 character",
+          },
+          maxLength:{
+            value:10,
+            message:"min 10 character",
+          }
+        }}
+        onChange={e=>setMobilenumber(e.target.value)}
+        value={mobilenumber}
+        ></input>
+      </form>
+      <button type="sumit" onClick={handle}>Get otp</button>
     </div>
   );
 }
