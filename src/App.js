707831361aa8df9d58a3ce75f879f0c3fc6796e4
aditya verma <adityavermasuper.12527@gmail.com>
2023-05-05 09:37:38 +0530

    axios dog show

--- src/App.js
+++ src/App.js
@@ -1,64 +1,12 @@
-import { useState } from 'react';
+import { useState } from 'react'
 import './App.css';
+import  DogImgs from './dog'
 
 function App() {
- const [mobilenumber,setMobilenumber] =useState()
-  
- function handle(){
-  if(mobilenumber < 999967896 || mobilenumber >= 56567890342){
-    alert ("Invalid mobile number")
-    console.error("invalid number")
-    return;
-  }
-  fetch( "https://cdn-api.co-vin.in/api/v2/auth/public/generateOTP",{
-    method:"POST",
-    headers:{
-      "content-Type": "application/json"
-    },
-    body: JSON.stringify({
-      mobile: mobilenumber
-    })
-  })
 
-     .then((res)=> res.json())
-     .then((data)=> {
-      setMobilenumber(" ");
-      alert("succesfully")
-      console.log(data);
-     })
-     .catch(error=>{
-      alert("plase retry, somthing went wrong")
-      console.error(error);
-     });
-      }
   return (
     <div className='App'>
-      <form
-      onSubmit={e=> e.preventDefault()}
-      noValidate
-      autoComplete='off'
-      
-      >
-        <input type="number" placeholder='mobile number' 
-        validation={{
-          required:{
-            value:"true",
-            message:"required",
-          },
-          minLength:{
-            value:10,
-            message: "min 10 character",
-          },
-          maxLength:{
-            value:10,
-            message:"min 10 character",
-          }
-        }}
-        onChange={e=>setMobilenumber(e.target.value)}
-        value={mobilenumber}
-        ></input>
-      </form>
-      <button type="sumit" onClick={handle}>Get otp</button>
+     < DogImgs/>
     </div>
   );
 }
